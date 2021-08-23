0445f92b530d0b493af30fa12263801baee915a7
Matheus <sampaio.brenner@gmail.com>
2021-08-22 21:28:17 -0300

    Adicionando injeção de dependência ao projeto

--- HandsOfWork/Program.cs
+++ HandsOfWork/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Windows.Forms;
 
 namespace HandsOfWork
@@ -8,9 +9,16 @@ namespace HandsOfWork
         [STAThread]
         private static void Main()
         {
+            var services = new ServiceCollection();
+            services.AddScoped<Form1>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            var form1 = serviceProvider.GetRequiredService<Form1>();
+            Application.Run(form1);
         }
     }
 }
