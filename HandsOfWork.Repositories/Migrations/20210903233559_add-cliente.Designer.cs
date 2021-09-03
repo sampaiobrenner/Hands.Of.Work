62643acd8eaed0bbf4a9cbf3c145552fc7d5b788
Matheus Sampaio <sampaio.brenner@gmail.com>
2021-09-03 20:36:23 -0300

    Criando migration add cliente

--- HandsOfWork.Repositories/Migrations/20210903233559_add-cliente.Designer.cs
+++ HandsOfWork.Repositories/Migrations/20210903233559_add-cliente.Designer.cs
@@ -3,14 +3,16 @@ using HandsOfWork.Repositories.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HandsOfWork.Repositories.Migrations
 {
     [DbContext(typeof(HandsOfWorkContext))]
-    partial class HandsOfWorkContextModelSnapshot : ModelSnapshot
+    [Migration("20210903233559_add-cliente")]
+    partial class addcliente
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HandsOfWork.Repositories/Migrations/20210903233559_add-cliente.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HandsOfWork.Repositories.Migrations
+{
+    public partial class addcliente : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
